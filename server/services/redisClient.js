f4fac46a3de924b773e1d55e35e3170ad0188c25
Peter <peterjang360@gmail.com>
2023-09-17 23:21:35 -0400

    log redis url

--- server/services/redisClient.js
+++ server/services/redisClient.js
@@ -9,7 +9,9 @@ const initRedisClient = async () => {
   if (!client) {
     console.log('Checking REDIS_URL:', process.env.REDIS_URL);
     
-    client = redis.createClient(process.env.REDIS_URL);
+    client = redis.createClient({
+      url: process.env.REDIS_URL
+    });
 
     client.on("connect", () => {
       console.log("Connected to Redis");
